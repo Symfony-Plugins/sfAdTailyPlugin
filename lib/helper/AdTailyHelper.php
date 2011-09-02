cf7fabe5c9afc59c8fcf4d6fa25dbc0fe337b1ef
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-09-02 03:54:12 -0500

    [sfAdTailyPlugin] rebuild helper
    git-svn-id: http://svn.symfony-project.com/plugins/sfAdTailyPlugin/trunk@32982 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/AdTailyHelper.php
+++ lib/helper/AdTailyHelper.php
@@ -1,8 +1,12 @@
 <?php
 
 function at_insert_ad_taily_ad() {
-	if (sfConfig::get('app_ad_taily_plugin_enabled', false) === false) return;
-	if (($code = sfConfig::get('app_ad_taily_plugin_code')) === null) return;
+	if (($code = sfConfig::get('app_ad_taily_plugin_code')) === null) {
+		sfContext::getInstance()->getLogger()->alert('{sfAdTailyPlugin} AdTaily Code is not set!');
+		return;
+	}
 
-	echo "<script type=\"text/javascript\" id=\"AdTaily_Widget\" src=\"http://static.adtaily.pl/widget.js#{$code}\"></script>";
+	if (sfConfig::get('app_ad_taily_plugin_enabled') !== false && $code !== null) {
+		echo str_replace('{$code}', $code, sfConfig::get('app_ad_taily_plugin_template', ''));
+	}
 }
